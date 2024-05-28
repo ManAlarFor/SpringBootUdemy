ef42a185a2624aaeaadeafd5b03c6f8d9e28d4c9
Manuel <alarmanuel16@gmail.com>
2024-05-28 10:21:25 +0200

    Seccion 18: "Backend App"

---
+++ backend-products/src/main/java/com/manuel/curso/springboot/backend/backend_products/repositories/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.manuel.curso.springboot.backend.backend_products.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.manuel.curso.springboot.backend.backend_products.entities.Product;
+
+@RepositoryRestResource(path = "products")
+public interface ProductRepository extends CrudRepository<Product, Long> {
+
+
+
+}
